e848f3bae3deed66ddc4bcd0db98b7c8f38117b0
Bayard <bayardneville@gmail.com>
2016-12-11 00:29:28 -0500

    fixed radio buttons and made script pull values out of form

--- script.js
+++ script.js
@@ -2,13 +2,22 @@ var key = "AIzaSyAzKcjLpeaBQ3G-Fb0r8X318XVDSI9kbhQ"
 
 $("#user-info-form").submit(function(event) {
   event.preventDefault();
-  var mode = "transit";
+  var mode = $('input[name=mode]:checked').val();;
   var origin = $("#homeAddress").val();
   var destination = $("#workAddress").val();
-  var request = $.get($(this).attr("action"), {mode: mode, origin: origin, destination: destination, key: key});
+
+  var request = $.get($(this).attr("action"), {
+    key: key,
+    mode: mode,
+    origin: origin,
+    destination: destination
+  });
+
   request.done(function(data) {
-    alert(data);
     alert(data["routes"][0]["legs"][0]["duration"]["text"]);
   });
-});
 
+  request.fail(function(data) {
+    alert("Request failed");
+  });
+});
