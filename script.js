2e6cfe492c2feba398452553bcb068e03a193652
Bayard <bayardneville@gmail.com>
2016-12-13 00:51:13 -0500

    added basic nightlife/neighborhood querying

--- script.js
+++ script.js
@@ -1,28 +1,53 @@
+homeAutocomplete = new google.maps.places.Autocomplete(document.getElementById('homeAddress'), {
+  types: ['address'],
+});
+workAutocomplete = new google.maps.places.Autocomplete(document.getElementById('workAddress'), {
+  types: ['address'],
+});
+
+
 var mapOptions = {
   zoom: 4,
   center: {lat: 40.0, lng: -95.0}
 }
 
 var map = new google.maps.Map(document.getElementById('map'), mapOptions);
+var placeService = new google.maps.places.PlacesService(map);
+var geocoder = new google.maps.Geocoder;
 var dirService = new google.maps.DirectionsService();
 var dirDisplay = new google.maps.DirectionsRenderer();
 dirDisplay.setMap(map);
 
 $("#user-info-form").submit(function(event) {
   event.preventDefault();
+  var slider = $('#hood_type').val();
   var travelMode = $('input[name=mode]:checked').val();;
-  var origin = $("#homeAddress").val();
-  var destination = $("#workAddress").val();
+  var home = $("#homeAddress").val();
+  var work = $("#workAddress").val();
 
-  var request = {
-    origin: origin,
-    destination: destination,
-    travelMode: travelMode,
-  }
-  dirService.route(request, function(response, status) {
-    if (status == 'OK') {
-      dirDisplay.setDirections(response);
-      alert(response.routes[0].legs[0].duration.text);
+  geocoder.geocode({address: home}, function(response, status) {
+    var homeLoc = response[0].geometry.location;
+    var placeRequest = {
+      keyword: "nightlife",
+      location: homeLoc,
+      radius: 1000
     }
+    placeService.radarSearch(placeRequest, function(response, status) {
+      alert(response.length);
+    });
   });
+
+  if(work != "") {
+    var dirRequest = {
+      origin: home,
+      destination: work,
+      travelMode: travelMode
+    }
+    dirService.route(dirRequest, function(response, status) {
+      if (status == 'OK') {
+        dirDisplay.setDirections(response);
+        alert(response.routes[0].legs[0].duration.text);
+      }
+    });
+  }
 });
